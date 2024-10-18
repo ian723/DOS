0c2e532274d0bcd4e4b2b1fad3c6577d2386a810
Ian Ndegwa <ndegwaian241@gmail.com>
2024-10-18 12:13:09 +0300

    message

--- app/src/components/Common/Header.vue
+++ app/src/components/Common/Header.vue
@@ -39,8 +39,11 @@
 
       <!-- Account and Tray (on the right) -->
       <div class="flex items-center space-x-4">
+        <router-link to="/" class="hover:underline">Home</router-link>
         <router-link to="/admin" class="hover:underline">Admin</router-link>
-        <router-link to="/customer" class="hover:underline">Customer</router-link>
+        <router-link to="/customer" class="hover:underline"
+          >Customer</router-link
+        >
         <router-link to="/account" class="hover:underline">Account</router-link>
         <router-link to="/tray" class="hover:underline">Tray</router-link>
       </div>
